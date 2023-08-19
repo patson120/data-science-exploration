a44354a1e954c577638fb3801bf9ced1e22fb7da
Patrick Kenne Tchinda <patrick1kenne@gmail.com>
2023-08-19 19:30:25 +0200

    Utilisattion  de notebook pour le scraping avec selenium

--- web_scraping/google/script.py
+++ web_scraping/google/script.py
@@ -7,19 +7,26 @@ import time
 #  Créer une variable pour déclencher le driver
 # driver = webdriver.Edge(keep_alive=True)
 driver = webdriver.Chrome(keep_alive=True)
-# driver.get('https://www.kubii.fr/')
+driver.get('https://www.kubii.fr/')
 # driver.get('http://127.0.0.1:5500/') # Exemple sur un application en local
 
-# # Récupération de la barre de recherche
-# search_bar = driver.find_element(By.CLASS_NAME, "ui-autocomplete-input")
+# Récupération de la barre de recherche
+search_bar = driver.find_element(By.CLASS_NAME, "ui-autocomplete-input")
 
-# search_bar.send_keys("Raspbarry pi")
+search_bar.send_keys("Raspbarry pi")
 
 
-# # Récupérer le bouton  pour lancer la recherche
-# search_btn = driver.find_element(By.ID, "search_widget").find_element(By.TAG_NAME, "button")
+# Récupérer le bouton  pour lancer la recherche
+search_btn = driver.find_element(By.ID, "search_widget").find_element(By.TAG_NAME, "button")
 
-# search_btn.click()
+search_btn.click()
+
+# Récupérer tout les noms des produits 
+all_titles = driver.find_elements(By.CSS_SELECTOR, "h3.product-title")
+
+# Parcourir la liste des produits
+for title in all_titles:
+    print(title.text)
 
 
 time.sleep(60*60*12)
